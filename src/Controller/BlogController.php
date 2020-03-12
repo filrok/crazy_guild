21431777bed33cfcbc61c6f262be3a15b50f6fea
david <david@dedi-agency.com>
2020-03-12 17:57:49 +0100

    New branche

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,1 +1,22 @@
 <?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+use App\Services\ComplexObject;
+use App\Services\MailLogger;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog_list")
+     */
+    public function list()
+    {
+        $mailLogger = new MailLogger();
+        dump($mailLogger->sendMail());
+
+        return $this->render('base.html.twig');
+    }
+}
